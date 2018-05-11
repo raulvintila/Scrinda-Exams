92a6819d8ac88663a737e1122a9ed1d295294aaf
Raul Vintila <ioan.raul.vintila@gmail.com>
2018-05-11 18:05:15 +0300

    Refactored homepage view to use a template

--- examsdb/tests.py
+++ examsdb/tests.py
@@ -1,10 +1,20 @@
 from django.urls import resolve
 from django.test import TestCase
 from examsdb.views import home_page
-
+from django.template.loader import render_to_string
 
 class HomePageTest(TestCase):
 
-	def test_root_url_resolves_to_home_page_view(self):
-		found = resolve('/')
-		self.assertEqual(found.func, home_page)
+	def test_uses_home_template(self):
+		response = self.client.get('/')
+		self.assertTemplateUsed(response, 'home.html')
+		
+	
+	def test_home_page_returns_correct_html(self):
+		
+		response = self.client.get('/')
+
+		html = response.content.decode('utf8')
+		self.assertTrue(html.startswith('<html>'))
+
+		self.assertTemplateUsed(response, 'home.html')
